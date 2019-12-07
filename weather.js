9bfd8edb01d7d9dd8ba8f65e977c9638085453f1
prakhar19970 <prakharshukla20160@gmail.com>
2019-12-07 12:18:19 +0530

    file modified

--- weather.js
+++ weather.js
@@ -1,32 +1,33 @@
 const { Common } = require("./common");
 const weatherData = [];
-class weather extends Common{
+class weather extends Common {
 
-static operateData(weatherData) {
-  let row = [];
-  let rowswithoutspaces=[]
-  let resultObject = {};
-  let mintempspread=0,day=0,x=0,y=0;
-  for (let i=2 ; i < weatherData.length;i++) {
-    row = weatherData[i].toString().trim().split(" ");
-    rowswithoutspaces=row.filter((data)=> data !== '');
-    x=String(rowswithoutspaces[1])
-    y=String(rowswithoutspaces[2])
-    if (x.includes("*")){
-        x = x.replace("*", "");   
-    }else {
-              y = y.replace("*", "");
-            }
-resultObject=weather.findingMinimum(mintempspread,x,y,row[0])
-mintempspread=resultObject.mindifference;
+  static operateData(weatherData) {
+    let row = [];
+    let rowswithoutspaces = []
+    let resultObject = {};
+    let mintempspread = 0, day = 0, x = 0, y = 0;
+    for (let i = 2; i < weatherData.length; i++) {
+      row = weatherData[i].toString().trim().split(" ");
+      rowswithoutspaces = row.filter((data) => data !== '');
+      x = String(rowswithoutspaces[1])
+
+      y = String(rowswithoutspaces[2])
+      if (x.includes("*")) {
+        x = x.replace("*", "");
+      } else {
+        y = y.replace("*", "");
+      }
+      resultObject = weather.findingMinimum(mintempspread, x, y, row[0])
+      mintempspread = resultObject.mindifference;
+    }
+    console.log(`minimum temperature spread for day ${resultObject.attribute} = ${resultObject.mindifference}`);
   }
-  console.log(`minimum temperature spread for day ${resultObject.attribute} = ${resultObject.mindifference}`);
-}
 }
 
 weather.readfile('weather.dat')
   .then(result => {
-      //console.log(result);
+    //console.log(result);
     weather.operateData(result)
   })
   .catch(error => {

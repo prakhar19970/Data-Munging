1a43609c14abd8f0887e401a651d10b819066dad
prakhar19970 <prakharshukla20160@gmail.com>
2019-12-07 12:18:05 +0530

    file modified

--- football.js
+++ football.js
@@ -1,17 +1,17 @@
 const { Common } = require("./common");
 let footballData = [];
 class Football extends Common {
-  
- static operateData(footballData) {
+
+  static operateData(footballData) {
     let row = [];
     let matchdata = [];
-    let mindifference = 0,x = 0,y = 0;
+    let mindifference = 0, x = 0, y = 0;
     let resultObject = {};
     for (let i = 1; i < footballData.length; i++) {
       row = footballData[i].toString().split(" ");
       matchdata = row.filter(data => data !== "");
-     resultObject=Football.findingMinimum(mindifference,String(matchdata[6]),String(matchdata[8]),matchdata[1])
-     mindifference=resultObject.mindifference;
+      resultObject = Football.findingMinimum(mindifference, String(matchdata[6]), String(matchdata[8]), matchdata[1])
+      mindifference = resultObject.mindifference;
     }
     console.log(`Team ${resultObject.attribute} has minimum difference = ${resultObject.mindifference}`);
   }
@@ -19,7 +19,7 @@ class Football extends Common {
 
 Football.readfile('football.dat')
   .then(result => {
-      //console.log(result);
+    //console.log(result);
     Football.operateData(result)
   })
   .catch(error => {

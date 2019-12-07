bd43a2f617a612c7f68006d9478667aa1349b4c0
prakhar19970 <prakharshukla20160@gmail.com>
2019-12-07 12:17:47 +0530

    file modified

--- common.js
+++ common.js
@@ -1,40 +1,38 @@
 const fs = require("fs");
 const csv = require("fast-csv");
-const   dataRead=[]
-let object ={ 'mindifference' : 0, 'attribute' :''}
+const dataRead = []
+let object = { 'mindifference': 0, 'attribute': '' }
 class Common {
 
-    
-static readfile(path)
-   {
-    
-  
+
+  static readfile(path) {
+
+
     return new Promise((resolve, reject) => {
-    fs.createReadStream(path)
-    .pipe(csv.parse({ headers: false }))
-    .on("data", row => {
-      dataRead.push(row);
-    })
-    .on("end", () => {
-       // console.log(dataRead)
-     resolve (dataRead)
-    })
-   });
-    }
+      fs.createReadStream(path)
+        .pipe(csv.parse({ headers: false }))
+        .on("data", row => {
+          dataRead.push(row);
+        })
+        .on("end", () => {
+          // console.log(dataRead)
+          resolve(dataRead)
+        })
+    });
+  }
 
 
-    static findingMinimum(mindifference,x,y,attribute)
-    {
-      
-        let difference = Math.abs(Number(x - y));
-        if (mindifference === 0 || mindifference > difference) {
-            object.mindifference = difference;
-            object.attribute=attribute;
-            
+  static findingMinimum(mindifference, x, y, attribute) {
+
+    let difference = Math.abs(Number(x - y));
+    if (mindifference === 0 || mindifference > difference) {
+      object.mindifference = difference;
+      object.attribute = attribute;
+
     }
     return object;
+  }
 }
-}
-module.exports={
- Common   
+module.exports = {
+  Common
 }
